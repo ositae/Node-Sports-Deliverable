8d1e2ad9b215b2efb0159286398ff1e9fa59a397
Osita Enweronye <113301069+ositae@users.noreply.github.com>
2023-10-17 00:35:28 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express'); // third party node module
+
+const app = express(); //  instance of the app
+
+const fs = require('fs');
+
+
+
+
+// test using the core module (fs)
+
+fs.readFile('players.txt', 'teams.txt', 'utf8', (error, data) => {
+
+    if(error){
+
+        console.log("Error: ", error);
+
+        } else{
+
+            console.log(data);
+
+        };
+
+})
+
+
+
+
+
+
+// using core modules and req.query
+
+// Example: localhost:8000/read?something=story
+
+// {something: 'story'}
+
+app.get('/read', (req, res) => {
+
+    // grab a query string
+
+    // pass the query string into the fs function
+
+    // return the data that comes from the txt file
+
+    let element = req.query.something; // players, teams
+
+    fs.readFile(`${element}.txt`, 'utf-8', (error, data) => {
+
+        if (error) {
+
+            return res.json({message: 'There is an issue. Try again!'})
+
+        } else {
+
+            return res.json({message: data});
+
+        }
+
+    });
+
+})
+
+// set up a PORT number, and listen for server
+
+const PORT = process.env.PORT || 8000;
+
+
+
+
+app.listen(PORT, () => {
+
+    console.log('Server is runing on PORT', PORT);
+
+});
